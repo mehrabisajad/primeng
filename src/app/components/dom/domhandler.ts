0b0663f5de01bbc63eab2ee5cd4a42489b6964ba
sssh <ssshobeiry@gmail.com>
2025-06-09 09:11:53 +0330

    fix(TabView): fix tabview inkbar first position bug in dialog

--- src/app/components/dom/domhandler.ts
+++ src/app/components/dom/domhandler.ts
@@ -481,7 +481,8 @@ export class DomHandler {
 
         return {
             top: rect.top + (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0),
-            left: rect.left + (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0)
+            left: rect.left + (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0),
+            right: rect.left + (window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0) + rect.width
         };
     }
 
